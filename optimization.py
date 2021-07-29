083a7ab45eddc2ef58a72bca4599fefa565ef16d
Eve <yydxhhuyi@gmail.com>
2021-07-28 20:44:53 -0400

    draft

--- optimization.py
+++ optimization.py
@@ -14,16 +14,21 @@ y.value = np.ones(n) * 100
 c = np.random.random((m, n))
 B = 5000
 
-x = cp.Variable((m, n), nonneg=True)
 
-obj = cp.Minimize(cp.sum(cp.multiply(gamma, cp.inv_pos(cp.sqrt(x @ y))) ) )
+def solver(wD, wS, y, u, BD, BS, gamma = 0, hard=False):
 
-constraints = [0 <= x, x <= 1,
+    x = cp.Variable((m, n), nonneg=True)
+
+    obj = cp.Minimize(cp.sum(cp.inv_pos(cp.sqrt(x @ y)) ) )
+
+    constraints = [0 <= x, x <= 1,
                cp.sum(c.T @ x @ y) <= B,
                cp.sum(x, 0) == 1]
 
-prob = cp.Problem(obj, constraints)
-prob.solve()  # Returns the optimal value.
-print("status:", prob.status)
-print("optimal value", prob.value)
-print("optimal var", x.value)
+    prob = cp.Problem(obj, constraints)
+    prob.solve()  # Returns the optimal value.
+    print("status:", prob.status)
+    print("optimal value", prob.value)
+    print("optimal var", x.value)
+
+solver(None, None, None, None, None)
