21c984af2f28d64ce91a4893d8ba71c62b163b40
Eve <yydxhhuyi@gmail.com>
2021-07-29 22:55:48 -0400

    refactor and fix some bugs

---
+++ functions.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cvxpy as cp
+
+class Trace(object):
+    def __init__(self, m, n, seed=1):
+        np.random.seed(seed)
+        data = np.loadtxt('trace_data/Lumos5G-v1.0/Lumos5G-v1.0.csv', delimiter=',', skiprows=1,
+                          usecols=(0, 15)).astype(int)
+        self.traces = []
+        self.m = m
+        self.n = n
+        for run in np.unique(data[:, 0]):
+            trace = data[data[:, 0] == run, 1]
+            trace = 1 / (trace + 1)
+            self.traces.append(trace)
+        self.average = [np.average(trace) for trace in self.traces]
+        self.trace_selection = np.random.choice(len(self.traces), (m, n))
+        self.trace_counter = np.zeros_like(self.trace_selection)
+        for i in range(m):
+            for j in range(n):
+                self.trace_counter[i, j] = np.random.choice(len(self.traces[self.trace_selection[i, j]]))
+
+    def generate(self):
+        rt = np.zeros((self.m, self.n))
+        for i in range(self.m):
+            for j in range(self.n):
+                rt[i, j] = self.traces[self.trace_selection[i, j]][self.trace_counter[i, j]]
+                self.trace_counter[i, j] += 1
+                if self.trace_counter[i, j] == len(self.traces[self.trace_selection[i, j]]):
+                    self.trace_counter[i, j] = 0
+        return rt
+
+    def avg(self):
+        rt = np.zeros((self.m, self.n))
+        for i in range(self.m):
+            for j in range(self.n):
+                rt[i, j] = self.average[self.trace_selection[i, j]]
+        return rt
+
+
+def objective(x, y, mu, wD, wS, m, n, gamma):
+    a = np.multiply(x[0, :], y)
+    b = x[1:, :] @ y
+    c = mu @ b
+    d = np.sum(1 / np.sqrt(a + wD)) \
+        + np.sum(1 / np.sqrt(np.sqrt(b + wS)))
+    e = np.sum(c) / m / n
+    return d + gamma * e, np.average(a), np.average(b), np.average(c), np.average(d), gamma * np.average(e)
+
+
+def optimization(m, n, wD, wS, y, mu, BD, BS, gamma, hard, CD=None):
+    if hard:
+        gamma = 0
+        assert CD is not None, 'Hard CD constraint missing'
+
+    Y = cp.Parameter((m,), nonneg=True)
+    Y.value = y
+
+    x = cp.Variable((n + 1, m), nonneg=True)
+
+    obj = cp.sum(cp.inv_pos(cp.sqrt(cp.multiply(x[0, :], y) + wD))) \
+          + cp.sum(cp.inv_pos(cp.sqrt(x[1:, :] @ y + wS))) + gamma * cp.sum(mu @ (x[1:, :] @ y)) / m / n
+
+    constraints = [0 <= x, x <= 1,
+                   cp.multiply(x[0, :], y) <= BD,
+                   x[1:, :] @ y <= BS,
+                   cp.sum(x, 0) == 1]
+
+    if hard:
+        constraints.append(mu @ (x[1:, :] @ y) <= CD)
+
+    prob = cp.Problem(cp.Minimize(obj), constraints)
+    prob.solve()  # Returns the optimal value.
+    return x, prob
--- optimization_soft.py
+++ optimization_soft.py
@@ -1,22 +1,25 @@
 from functions import *
 
-seed = 4
+
+seed = 1
 N = 3  # number of experiments
 T = 500  # rounds per experiment
+gamma = 10
 rs = 1
+
 np.random.seed(seed)
 
-m = 20  # number of devices
+m = 20 # number of devices
 n = 5  # number of servers
 
-wD = np.random.randint(1, 4, m)
-wS = np.random.randint(1, 4, n)
+wD = np.random.randint(1 ,4 ,m )
+wS = np.random.randint(1 ,4 ,n )
 
-BD = np.random.uniform(100, 400, m)
+BD = np.random.uniform(50, 80, m)
 BS = np.random.uniform(100, 300, n)
-CD = np.random.uniform(300, 600, m)
+CD = np.random.uniform(100, 200, m)
 
-# f(x, y, mu)
+
 def f(x, y, mu, gamma=1):
     return objective(x, y, mu, wD, wS, m, n, gamma)
 
@@ -26,46 +29,33 @@ def oracle(y, mu, gamma=0, hard=False):
     return x.value, prob.value, prob.status
 
 
-
 trace_gen = Trace(m, n, seed)
-
 reg = np.zeros((N, T))
 
 # statistics
 stats = np.zeros((N, T, 3))
 
 for u in range(N):
-    mu = np.random.rand(m, n)
-    # mu = trace_gen.avg()
+    # mu = np.random.rand(m, n)
+    mu = trace_gen.avg()
     mu_hat = np.zeros_like(mu)  # empirical mean
     T_ij = np.ones_like(mu)  # total number of times arm (i,j) is played
     for t in range(T):
         y = np.random.uniform(80, 120, m).astype(int)
-        x_opt, f_opt, status = oracle(y, mu, hard=True)
+        x_opt, f_opt, status = oracle(y, mu, gamma)
 
         if 'optimal' not in status:
             print('Solution infeasible')
             break
 
-        fv, *_, dv, ev = f(x_opt, y, mu)
+        fv, *_, dv, ev = f(x_opt, y, mu, gamma)
         stats[u, t] = np.array([fv, dv, ev])
 
         rho_ij = np.sqrt(3 * np.log(t + 1) / (2 * T_ij)) * rs
         mu_bar = np.max(mu_hat - rho_ij, 0)  # LCB
 
-        x_tmp, f_t, status = oracle(y, mu_bar, hard=True)
-
-        # mapping from x_tmp to x_t
-        x_t = np.zeros((n + 1, m))
-        for i in range(m):
-            cost = mu.dot(x_tmp[1:, i]).dot(y)
-            if cost > CD[i]:
-                x_t[1:, i] = CD[i] / cost * x_tmp[1:, i]
-                x_t[0, i] = 1 - np.sum(x_t[1:, i])
-            else:
-                x_t[:, i] = x_tmp[:, i]
-
-        f_t, *_ = f(x_t, y, mu, 0)
+        x_t, f_t, status = oracle(y, mu_bar, gamma)
+        f_t, *_ = f(x_t, y, mu, gamma)
 
         # sample j based on x_t[i], observe c_ij, update mu_hat[i,j]
         c = trace_gen.generate()
@@ -73,10 +63,10 @@ for u in range(N):
             j = np.random.choice(n + 1, p=x_t[:, i])
             if j != 0:
                 j -= 1
-                c_ij = int(np.random.rand() < mu[i, j])
+                # c_ij = int(np.random.rand() < mu[i, j])
                 # a = np.random.rand() * 3
                 # c_ij = np.random.beta(a, a * (1-mu[i, j])/mu[i, j]) # beta distribution
-                # c_ij = c[i, j]  # trace
+                c_ij = c[i,j] #trace
 
                 T_ij[i, j] += 1
                 mu_hat[i, j] += (c_ij - mu_hat[i, j]) / T_ij[i, j]
@@ -85,12 +75,11 @@ for u in range(N):
         reg[u, t] = f_t - f_opt
 
 plt.plot(np.cumsum(reg, axis=1).T)
-f_avg = np.average(stats[:, :, 0], 1)
-obj_avg = np.average(stats[:, :, 1], 1)
-cons_avg = np.average(stats[:, :, 2], 1)
+f_avg = np.average(stats[:,:,0], 1)
+obj_avg = np.average(stats[:,:,1], 1)
+cons_avg = np.average(stats[:,:,2], 1)
 legend = []
 for i in range(N):
-    legend.append('obj=%.2f, cst=%.2f' % (obj_avg[i], cons_avg[i]))
+    legend.append('obj=%.2f, cst=%.2f' %(obj_avg[i], cons_avg[i]))
 plt.legend(legend)
-plt.title('Hard, rs=%.2f, mu_avg=%.2f' %(rs, np.average(mu)))
 plt.show()
